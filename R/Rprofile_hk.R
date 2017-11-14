ef1ac651d6d34f27cc2379d6d6c59b382c5922ad
holgerman <holgerkirsten@web.de>
2017-11-14 19:11:20 +0100

    fix qqPlotMinP 2

--- R/Rprofile_hk.R
+++ R/Rprofile_hk.R
@@ -1103,23 +1103,26 @@ qq_conf = function(x, df=1, x.max = "auto",
 }
 
 ## qq plot fuer minimum p
-qqPlotMinP =  function(x, number_of_tests = 3, xlab = expression(Expected ~
-                                                                    ~-log[10](italic(p[min]))), ylab = expression(Observed ~
-                                                                                                                    ~-log[10](italic(p[min]))), main = deparse(substitute(x)),
-                        las = par("las"), subtitle = paste0("(Considering minimum of ",
-                                                            number_of_tests, " independent p-value-series)"), envelope = 0.95,
-                        col = palette()[1], col.lines = palette()[2], lwd = 2, pch = 16,
-                        cex = par("cex"), labels = if (!is.null(names(x))) names(x) else seq(along = x),
-                        id.method = "y", id.n = if (id.method[1] == "identify") Inf else 0,
-                        id.cex = 1, id.col = palette()[1], id.location = "lr", grid = TRUE,...)
+qqPlotMinP = function (x, number_of_tests = 3, xlab = expression(Expected ~
+                                                                                ~-log[10](italic(p[min]))), ylab = expression(Observed ~
+                                                                                                                                ~-log[10](italic(p[min]))), main = deparse(substitute(x)),
+                                    las = par("las"), subtitle = paste0("(Considering minimum of ",
+                                                                        number_of_tests, " independent p-value-series)"), envelope = 0.95,
+                                    col = palette()[1], col.lines = palette()[2], lwd = 2, pch = 16,
+                                    cex = par("cex"), labels = if (!is.null(names(x))) names(x) else seq(along = x),
+                                    id.method = "y", id.n = if (id.method[1] == "identify") Inf else 0,
+                                    id.cex = 1, id.col = palette()[1], id.location = "lr", grid = TRUE,
+                                    ...)
 {
-  rows = 10000
-  bsp = data.table(a = runif(rows),b = runif(rows),c = runif(rows), num = 1:rows)
-  bsp[,mini := min(.SD), by  = num]
-  bsp
-  bsp[,qqPlotMinP(mini, ylim = c(0,7))]
+  #  inspired by car::qqPlot and qq_conf
 
 
+  #rows = 10000
+  # bsp = data.table(a = runif(rows), b = runif(rows), c = runif(rows),
+  #                  num = 1:rows)
+  # bsp[, `:=`(mini, min(.SD)), by = num]
+  # bsp
+  # bsp[, qqPlotMinP(mini, ylim = c(0, 7))]
   line <- "identity"
   good <- !is.na(x)
   ord <- order(x[good])
@@ -1138,6 +1141,7 @@ qqPlotMinP =  function(x, number_of_tests = 3, xlab = expression(Expected ~
   ord.x_plot = -log10(ord.x)
   plot(z_plot, ord.x_plot, type = "n", xlab = xlab, ylab = ylab,
        main = main, las = las, ...)
+  # plot(z_plot, ord.x_plot, type = "n", xlab = xlab, ylab = ylab, main = main, las = las)
   if (grid) {
     grid(lty = 1, equilogs = F)
     box()
@@ -1149,33 +1153,29 @@ qqPlotMinP =  function(x, number_of_tests = 3, xlab = expression(Expected ~
     b = 1
     abline(0, 1, col = col.lines, lwd = lwd)
   }
-  conf <- if (envelope == FALSE)
-    0.95
-  else envelope
-
-  # zz <- qnorm(1 - (1 - conf)/2)
-  # SE <- (b/dbeta(z, shape1 = 1, shape2 = number_of_tests)) *
-  #   sqrt(P * (1 - P)/n)
-  # fit.value <- a + b * z
-  # upper <- fit.value + zz * SE
-  # lower <- fit.value - zz * SE
-
-
-  ## hier von qq_norm geklaut und adaptiert, glaube, die nutzt hier die beziehung der beta zur stetigen Gleichverteilung aus
-  upper =  qbeta(p = qbeta(0.975, 1:n, n:1), shape1 = 1, shape2 = number_of_tests)  ##
-  lower =  qbeta(p = qbeta(0.025, 1:n, n:1), shape1 = 1, shape2 = number_of_tests)
-
-
+  conf <- if (envelope == FALSE) .95 else envelope
+  upper = qbeta(p = qbeta(p = (1 + conf)/2, 1:n, n:1), shape1 = 1, shape2 = number_of_tests)
+  lower = qbeta(p = qbeta(p = (1 - conf)/2, 1:n, n:1), shape1 = 1, shape2 = number_of_tests)
   lowergr0 = lower > 0
   table(lowergr0)
-
   if (envelope != FALSE) {
     lines(z_plot, -log10(upper), lty = 2, lwd = lwd, col = col.lines)
     lines(z_plot[lowergr0], -log10(lower[lowergr0]), lty = 2,
           lwd = lwd, col = col.lines)
   }
-  car:::showLabels(z, ord.x, labels = ord.lab, id.method = id.method,
-                   id.n = id.n, id.cex = id.cex, id.col = id.col, id.location = id.location)
+
+  showLabels = function (x, y, labels = NULL, id.method = "identify", id.n = length(x),
+                         id.cex = 1, id.col = palette()[1], id.location = "lr", ...)
+  {
+    res <- NULL
+    id.method <- if (is.list(id.method))
+      id.method
+    else list(id.method)
+    for (meth in id.method) res <- c(res, showLabels1(x, y, labels,
+                                                      meth, id.n, id.cex, id.col, id.location, ...))
+    return(if (is.null(res)) invisible(res) else res)
+  }
+
   invisible(data.table::data.table(z, ord.x, upper, lower))
 }
 ### normality check
@@ -1873,5 +1873,5 @@ fdr_matrixEQTL <- function(p, N) {
 ##..................................................................................
 
 
-message( "\n******************************\nSuccessfully loaded toolboxH version 0.1.10")
+message( "\n******************************\nSuccessfully loaded toolboxH version 0.1.11")
 # Inspired from http://gettinggeneticsdone.blogspot.com/2013/06/customize-rprofile.html

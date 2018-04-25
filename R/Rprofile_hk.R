554f3f857e45ac12ae57356e19280c0e11e9c92d
holgerman <holgerkirsten@web.de>
2018-04-25 11:34:17 +0200

    Fixing load_obj

--- R/Rprofile_hk.R
+++ R/Rprofile_hk.R
@@ -175,19 +175,19 @@ cleverLoadinGlobalEnv = function(name, path, myenvir = .GlobalEnv, number_or_nam
 load_obj <- function(f, number_or_name = 1) ## load object number_or_name with numberin new environment to allow that it does not overwrite something with the same name
 {
   envextra <- new.env()
-
   nm <- load(f, envextra)
   message("found following objects:\n", paste(nm, collapse = "\n"))
-
-  if(is.numeric (number_or_name)) {
+  if (is.numeric(number_or_name)) {
     message("\nimported object ", nm[number_or_name])
     return(envextra[[nm[number_or_name]]])
-  } else  if(is.character (number_or_name)) {
-    obj_number = which(nm== number_or_name)
-    if(length(obj_number)==0) stop(paste("did not found object named ", number_or_name))
-    message("\nimported object ", nm[which(nm== number_or_name)])
-    return(envextra[nm[which(nm)== number_or_name]])
   }
+  else if (is.character(number_or_name)) {
+    obj_number = which(nm == number_or_name)
+    if (length(obj_number) == 0)
+      stop(paste("did not found object named ", number_or_name))
+    message("\nimported object ", nm[which(nm == number_or_name)])
+    return(envextra[[nm[obj_number]]])
+  } else stop("number or name must be either a valid number or a valid character naming one of the objects above")
 }
 
 
@@ -2922,5 +2922,5 @@ fdr_matrixEQTL <- function(p, N) {
 ##..................................................................................
 
 
-message( "\n******************************\nSuccessfully loaded toolboxH version 0.1.24")
+message( "\n******************************\nSuccessfully loaded toolboxH version 0.1.25")
 # Inspired from http://gettinggeneticsdone.blogspot.com/2013/06/customize-rprofile.html

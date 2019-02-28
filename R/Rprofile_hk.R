ce2a7c8a800e63d5cb4d82916b30e16233e56093
holgerman <holgerkirsten@web.de>
2019-02-28 10:27:06 +0100

    typo bugfix

--- R/Rprofile_hk.R
+++ R/Rprofile_hk.R
@@ -1013,7 +1013,7 @@ hypertest2 <- function(besondere,gezogene,hintergrund, more=T, unique=T){   #bes
   }
 
 
-h### confidence intervalls to p values see http://www.bmj.com/content/343/bmj.d2304
+### confidence intervalls to p values see http://www.bmj.com/content/343/bmj.d2304
 ci2p <- function (Est,u,l) {
   SE = (u - l)/(2*1.96)
   z = Est/SE

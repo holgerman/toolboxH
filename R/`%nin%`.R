3b29a4b770f1c7b5d90f052d481033e77456f6ae
Holger Kirsten <holgerkirsten@web.de>
2019-08-22 12:22:59 +0200

    vs.0.2.5 export %nin% function

--- R/`%nin%`.R
+++ R/`%nin%`.R
@@ -1,3 +1,17 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname %nin%
+#' @export
+
 ##..................................................................................
 ## data manipulation and handling
 ##..................................................................................

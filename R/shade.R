3c44cc50027ba40a78d18c165ed76b8e9e8ed4ad
Holger Kirsten <holgerkirsten@web.de>
2019-08-28 16:28:43 +0200

    fixing qq_conf namespace

---
+++ R/shade.R
@@ -0,0 +1,17 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param x1 PARAM_DESCRIPTION
+#' @param y1 PARAM_DESCRIPTION
+#' @param x2 PARAM_DESCRIPTION
+#' @param y2 PARAM_DESCRIPTION
+#' @param color PARAM_DESCRIPTION, Default: col.shade
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname shade
+#' @export
